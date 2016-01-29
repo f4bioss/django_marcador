32b32acba408d912bda8e8b98c47a7ef1452a751
Fabio Santana <f4bioss@gmail.com>
2016-01-29 08:23:39 -0200

    Create a view bookmark_list to display all bookmarks

--- marcador/views.py
+++ marcador/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from models import Bookmark
+
+def bookmark_list(request):
+    bookmarks = Bookmark.public.all()
+    context = {'bookmarks': bookmarks}
+    return render(request, 'marcador/bookmark_list.html', context)
+
